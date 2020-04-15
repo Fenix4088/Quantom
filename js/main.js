b16781d92e61314906aa5771a15ca6bb9ce1af27
Fenix <fenix4088@gmail.com>
2020-04-15 21:05:09 +0200

    Card with item is ready

--- js/main.js
+++ js/main.js
@@ -46,14 +46,35 @@ $(document).ready(function(){
 
 //   Click on icons inside cart item 
 
-const hartIcon = document.querySelector('.shop__item-icon-hart');
+const hartIcon = document.querySelectorAll('.shop__item-icon-hart');
+const cartIcon = document.querySelectorAll('.shop__item-icon-cart');
 
-hartIcon.addEventListener('click', function(){
-    hartIcon.classList.toggle('active');
+function clickOnIcons(icon) {
+    for(let item of icon) {
+    
+        item.addEventListener('click', function(){
+            this.classList.toggle('active');
+    
+      })
+    
+    }
+}
 
-  })
+clickOnIcons(hartIcon);
+clickOnIcons(cartIcon);
 
 
+//  Click on menu icin in a card with item and show specification list
 
+const cards = document.querySelectorAll('.shop__item');
 
-
+for (let item of cards){
+    item.addEventListener('click', function(event){
+        const itemMenuIcon = item.querySelector('.shop__item-icon-menu path');
+        const itemMenuList = item.querySelector('.shop__item-specification');
+        if(event.target == itemMenuIcon){
+            itemMenuIcon.classList.toggle('active');
+            itemMenuList.classList.toggle('shop__item-specification--show');
+        }
+    })
+}
